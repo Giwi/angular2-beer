47585ab2cf4c420e29bb309be124c70226474dda
Xavier MARIN <marin.xavier@gmail.com>
2016-10-05 23:06:58 +0200

    step 2

--- step-01/app/main.ts
+++ step-01/app/main.ts
@@ -1,4 +1,1 @@
 import {bootstrap} from 'angular2/platform/browser';
-import {HTTP_PROVIDERS} from 'angular2/http';
-
-// bootstrap(ContactsList, [HTTP_PROVIDERS, UserDataService]);
---
+++ step-02/app/main.ts
@@ -0,0 +1,4 @@
+import {bootstrap} from 'angular2/platform/browser';
+import {BeerList} from './beerList.component';
+
+bootstrap(BeerList,[]);
