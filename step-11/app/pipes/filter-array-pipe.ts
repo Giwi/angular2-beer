eb88be4baf8eee7c281e080470b274c7ff9d8e65
Xavier MARIN <marin.xavier@gmail.com>
2016-10-08 11:05:09 +0200

    step-11

---
+++ step-11/app/pipes/filter-array-pipe.ts
@@ -0,0 +1,19 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({name: 'filter'})
+export class FilterArrayPipe implements PipeTransform {
+    transform(value, args) {
+        if (!args || !args[0]) {
+            return value;
+        } else if (value) {
+            return value.filter(item => {
+                for (let key in item) {
+                    if ((typeof item[key] === 'string' || item[key] instanceof String) &&
+                        (item[key].indexOf(args[0]) !== -1)) {
+                        return true;
+                    }
+                }
+            });
+        }
+    }
+}
